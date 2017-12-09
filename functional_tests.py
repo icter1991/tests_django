2584145727b3e004cc01d523bb35a9bc08590000
icter1991 <33804650+icter1991@users.noreply.github.com>
2017-12-09 21:54:12 +0000

    0.0.6
    Functional test  is checking ability of adding element to the list

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,19 +18,28 @@ class NewVisitorTest(unittest.TestCase):
 
         # Zwrocila uwage, ze tytul strony i naglowek zawieraja slowo Listy.
         self.assertIn('Listy', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.fail('Listy', header_text)
+
+        # Od razu zostaje zachecona, aby wpisac rzecz do zrobienia.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Wpisz rzecz do zrobienia')
+
+        # W polu tekstowym wpisala "Kupic pawie piora"(hobby Edyty
+        # polega na tworzeniu ozdobnych przynent).
+        inputbox.send_keys('Kupic pawie piora')
+
+        # Po nacisnieciu klawisza Enter strona zostala uaktualniona i wyswietla
+        # "1: Kupic pawie piora" jako element listy rzeczy do zrobienia.
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Kupic pawie piora' for row in rows))
+
+        # Na stronie nadal znajduje sie pole tekstowe zachecajace do podania kolejnego zadania.
+        # Edyta wpisala "Uzyc pawich pior do zrobienia przynety" (Edyta jest niezwykle skrupulatna).
         self.fail('Zakonczenie testu!')
-
-    # Od razu zostaje zachecona, aby wpisac rzecz do zrobienia.
-
-    # W polu tekstowym wpisala "Kupic pawie piora"(hobby Edyty
-    # polega na tworzeniu ozdobnych przynent).
-
-    # Po nacisnieciu klawisza Enter strona zostala uaktualniona i wyswietla
-    # "1: Kupic pawie piora" jako element listy rzeczy do zrobienia.
-
-    # Na stronie nadal znajduje sie pole tekstowe zachecajace do podania kolejnego zadania.
-    # Edyta wpisala "Uzyc pawich pior do zrobienia przynety" (Edyta jest niezwykle skrupulatna).
-
+        
     # Strona zostala ponownie uaktualniona i teraz wyswietla dwa elementy na liscie rzeczy do zrobienia.
 
     # Edyta byla ciekawa, czy witryna zapamieta jej liste. Zwrocila uwage na wygenerowany dla niej
