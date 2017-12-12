afafb8096607b5fd975d6db477ce39aa181ddcde
icter1991 <33804650+icter1991@users.noreply.github.com>
2017-12-12 22:50:35 +0000

    0.1.3
    make Functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox(executable_path=r'C:\Users\Ninja\PycharmProjects\geckodriver.exe')
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edyta dowiedziała się o nowej, wspaniałej aplikacji w postaci listy rzeczy do zrobienia.
         # Postanowila wiec przejsc na strone glowna tej aplikacji.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Zwrocila uwage, ze tytul strony i naglowek zawieraja slowo Listy.
         self.assertIn('To-Do', self.browser.title)
@@ -64,7 +65,3 @@ class NewVisitorTest(unittest.TestCase):
 
     # Usatysfakcjonowana kladzie sie spac.
 
-if __name__ == '__main__':
-    unittest.main()
-
-browser.quit()
